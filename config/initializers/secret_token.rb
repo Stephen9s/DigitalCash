540347715b8f6fdd248e09ac4eb9119258a1f5a3
Stephen Neyens <neyens.s@gmail.com>
2012-10-27 13:42:29 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DigitalCash::Application.config.secret_token = '9a29e161bb835c3bf7b412b89a8eda414a28d65e19a9d08734d21ace50e5f6577de18f74e255da58fe5a03e6e6739ade405d93a1e31801dea2dec253a31c45c0'
